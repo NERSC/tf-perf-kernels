a3cdd1d628576a1ad57437f6dcdc363841df1436
Thorsten Kurth <thorstenkurth@me.com>
2019-09-18 13:46:06 -0700

    new scripts

--- scripts/run_gori_nsight.sh
+++ scripts/run_gori_nsight.sh
@@ -32,7 +32,7 @@ run_dir=$WORK/tf_cnn_kernels_2/runs/${SLURM_JOBID}
 mkdir -p ${run_dir}
 
 #copy relevant files
-cp conv2d_v2.py ${run_dir}/
+cp ../python/conv2d_v2.py ${run_dir}/
 
 #variables
 prec=16
@@ -40,12 +40,12 @@ batch_size=16
 data_format="NHWC"
 
 #net_params
-#net_params_1="VGG-1,224x224x3,3x3x3x64,1 ResNet50-1,224x224x3,7x7x3x64,2 VGG-2,224x224x64,3x3x64x128,2 VGG-3,112x112x128,3x3x128x256,2 ResNet50-2,112x112x64,3x3x64x64,2"
+net_params="VGG-1,224x224x3,3x3x3x64,1 ResNet50-1,224x224x3,7x7x3x64,2 VGG-2,224x224x64,3x3x64x128,2 VGG-3,112x112x128,3x3x128x256,2 ResNet50-2,112x112x64,3x3x64x64,2"
 #net_params_2="ResNet50-2,112x112x64,3x3x64x128,2 ResNet50-2,112x112x64,3x3x64x256,2 ResNet50-2,112x112x64,3x3x64x512,2 ResNet50-2,112x112x64,7x7x64x64,2 ResNet50-2,112x112x64,9x9x64x64,2"
 #net_params_3="ResNet50-2,112x112x64,3x3x64x128,1 ResNet50-2,112x112x64,3x3x64x256,1 ResNet50-2,112x112x64,3x3x64x512,1 ResNet50-2,112x112x64,7x7x64x64,1 ResNet50-2,112x112x64,9x9x64x64,1"
 #net_params_4="ResNet50-2,112x112x64,3x3x64x128,3 ResNet50-2,112x112x64,3x3x64x256,3 ResNet50-2,112x112x64,3x3x64x512,3 ResNet50-2,112x112x64,7x7x64x64,3 ResNet50-2,112x112x64,9x9x64x64,3"
 #net_params="ResNet50-2,112x112x64,3x3x64x64,2 ResNet50-2,112x112x64,3x3x64x64,3"
-net_params="ResNet50-2,112x112x64,3x3x64x128,2"
+#net_params="ResNet50-2,112x112x64,3x3x64x128,2"
 
 
 #step in
@@ -53,7 +53,33 @@ cd ${run_dir}
 
 #list of metrics
 #metrics=""
-metrics="smsp__sass_thread_inst_executed_op_fadd_pred_on.sum" #,smsp__sass_thread_inst_executed_op_fmul_pred_on.sum,smsp__sass_thread_inst_executed_op_ffma_pred_on.sum"
+metrics=\
+"
+time \
+sm__inst_executed_pipe_tensor_op_hmma.avg.pct_of_peak_sustained_active,\
+smsp__sass_thread_inst_executed_op_fadd_pred_on.sum,\
+smsp__sass_thread_inst_executed_op_fmul_pred_on.sum,\
+smsp__sass_thread_inst_executed_op_ffma_pred_on.sum,\
+smsp__sass_thread_inst_executed_op_hadd_pred_on.sum,\
+smsp__sass_thread_inst_executed_op_hmul_pred_on.sum,\
+smsp__sass_thread_inst_executed_op_hfma_pred_on.sum,\
+l1tex__t_sectors_pipe_lsu_mem_global_op_ld.sum,\
+l1tex__t_sectors_pipe_lsu_mem_global_op_st.sum,\
+l1tex__t_set_accesses_pipe_lsu_mem_global_op_atom.sum,\
+l1tex__t_set_accesses_pipe_lsu_mem_global_op_red.sum,\
+l1tex__t_set_accesses_pipe_tex_mem_surface_op_atom.sum,\
+l1tex__t_set_accesses_pipe_tex_mem_surface_op_red.sum,\
+l1tex__t_sectors_pipe_lsu_mem_local_op_ld.sum,\
+l1tex__t_sectors_pipe_lsu_mem_local_op_st.sum,\
+smsp__inst_executed_op_shared_ld.sum,\
+smsp__inst_executed_op_shared_st.sum,\
+lts__t_sectors_op_read.sum,\
+lts__t_sectors_op_write.sum,\
+dram__sectors_read.sum,\
+dram__sectors_write.sum,\
+lts__t_sectors_aperture_sysmem_op_read.sum,\
+lts__t_sectors_aperture_sysmem_op_write.sum\
+"
 
 #iterate over metrics
 for metric in ${metrics}; do
@@ -73,20 +99,22 @@ for metric in ${metrics}; do
 
             #get better metric name
             metricname=${metric//,/-}
-    
+
             #assemble profiling string
             if [ "${metric}" == "time" ]; then
-                profilestring="nv-nsight-cu-cli --profile-from-start off"
+                profilestring="nsys profile --trace=cublas,cuda,cudnn --capture-range=cudaProfilerApi --stats=true -f true"
+                metrictag="time"
                 #profilestring="nv-nsight-cu-cli"
             else
-                profilestring="nv-nsight-cu-cli --profile-from-start off --metrics ${metric}"
+                profilestring="nv-nsight-cu-cli --profile-from-start off --metrics ${metric} -f"
+                metrictag="metrics"
                 #profilestring="nv-nsight-cu-cli --metrics ${metric}"
             fi
-            profilestring=${profilestring}" -f -o profile.name_${name}.batchsize_${batch_size}.inputshape_${2}.kernelshape_${3}.stride_${4}.dataformat_${data_format}.fp${prec}.pass_${ctype}.metric_${metricname}.nvvp"
-	    #profilestring=${profilestring}" -f -o profile.name_${name}.batchsize_${batch_size}.inputshape_${2}.kernelshape_${3}.stride_${4}.dataformat_${data_format}.fp${prec}.pass_${ctype}.metric_${metricname}.nsight"
+            #profilestring=${profilestring}" -f -o profile.name_${name}.batchsize_${batch_size}.inputshape_${2}.kernelshape_${3}.stride_${4}.dataformat_${data_format}.fp${prec}.pass_${ctype}.metric_${metricname}"
+            profilestring=${profilestring}" -o profile.name_${name}.batchsize_${batch_size}.inputshape_${2}.kernelshape_${3}.stride_${4}.dataformat_${data_format}.fp${prec}.pass_${ctype}.metric_${metrictag}"
 
             #compute types
-            ${sruncmd} ${profilestring} $(which python) -u ./conv2d.py \
+            ${sruncmd} ${profilestring} $(which python) -u ./conv2d_v2.py \
                 --dtype float${prec} \
                 --data_format ${data_format} \
                 --input_tensor_shape ${batch_size} ${input_tensor_shape} \
@@ -95,8 +123,11 @@ for metric in ${metrics}; do
                 --num_warmups 5 \
                 --num_iterations 20 \
                 --compute_type ${ctype}
+
         done
+
     done
+
 done
 
 #copy results
