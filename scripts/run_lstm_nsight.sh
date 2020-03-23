5875ef2e7e4a3e6a575ca1b1f4e6a5aad2905eeb
Yunsong Wang <yunsongwang@lbl.gov>
2020-03-23 10:25:55 -0700

    Cleanups + add lstm script

--- scripts/run_lstm_nsight.sh
+++ scripts/run_lstm_nsight.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J conv2d_test
+#SBATCH -J lstm_test
 #SBATCH -t 02:00:00
 #SBATCH -C gpu
 #SBATCH --gres=gpu:8
@@ -11,14 +11,9 @@ module load python/3.7-anaconda-2019.07
 
 #activate env
 source activate py3.7-tf2-cuda-10.2.89
-#module load tensorflow/gpu-1.13.1-py36
-#module load tensorflow/gpu-2.0.0-beta-py36
 
 #rankspernode
 rankspernode=1
-
-#custome link
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PWD}/../lib
 
 #openmp stuff
 export OMP_NUM_THREADS=$(( 40 / ${rankspernode} ))
@@ -26,32 +21,25 @@ export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 sruncmd="srun -N ${SLURM_NNODES} -n $(( ${SLURM_NNODES} * ${rankspernode} )) -c $(( 80 / ${rankspernode} )) --cpu_bind=cores"
 
-
 #create run dir
-run_dir=$PWD/tf_cnn_kernels_nsight/runs/${SLURM_JOBID}
+run_dir=$PWD/tf_cnn_kernels_2/runs/${SLURM_JOBID}
 mkdir -p ${run_dir}
 
 #copy relevant files
-cp ../python/conv2d_v2.py ${run_dir}/
+cp ../python/lstm1d_v2.py ${run_dir}/
 
 #variables
 prec=16
-batch_size=64
+batch_size=16
 data_format="NHWC"
-
 #net_params
-#net_params="VGG-1,224x224x3,3x3x3x64,1 ResNet50-1,224x224x3,7x7x3x64,2 VGG-2,224x224x64,3x3x64x128,2 VGG-3,112x112x128,3x3x128x256,2 ResNet50-2,112x112x64,3x3x64x64,2"
-#net_params="ResNet50-2,112x112x64,3x3x64x128,2 ResNet50-2,112x112x64,3x3x64x256,2 ResNet50-2,112x112x64,3x3x64x512,2 ResNet50-2,112x112x64,7x7x64x64,2 ResNet50-2,112x112x64,9x9x64x64,2"
-#net_params="ResNet50-2,112x112x64,3x3x64x128,1 ResNet50-2,112x112x64,3x3x64x256,1 ResNet50-2,112x112x64,3x3x64x512,1 ResNet50-2,112x112x64,7x7x64x64,1 ResNet50-2,112x112x64,9x9x64x64,1"
-#net_params="ResNet50-2,112x112x64,3x3x64x128,3 ResNet50-2,112x112x64,3x3x64x256,3 ResNet50-2,112x112x64,3x3x64x512,3 ResNet50-2,112x112x64,7x7x64x64,3 ResNet50-2,112x112x64,9x9x64x64,3"
-#net_params="ResNet50-2,112x112x64,3x3x64x64,1 ResNet50-2,112x112x64,3x3x64x64,3"
-net_params="ResNet50-2,112x112x64,3x3x64x64,2"
+
+net_params="lstm-1,10x32x32,'lstm',50 lstm-2,10x64x64,'lstm',50 lstm-3,10x32x32,'lstm',30 lstm-4,10x64x64,'lstm',30 lstm-5,5x32x32,'lstm',30"
 
 #step in
 cd ${run_dir}
 
 #list of metrics
-#metrics=""
 metrics="sm__inst_executed_pipe_tensor_op_hmma.avg.pct_of_peak_sustained_active,\
 smsp__sass_thread_inst_executed_op_dadd_pred_on.sum,\
 smsp__sass_thread_inst_executed_op_dmul_pred_on.sum,\
@@ -110,7 +98,7 @@ for metric in ${metrics}; do
             profilestring=${profilestring}" -o profile.name_${name}.batchsize_${batch_size}.inputshape_${2}.kernelshape_${3}.stride_${4}.dataformat_${data_format}.fp${prec}.pass_${ctype}"
 
             #compute types
-            ${sruncmd} ${profilestring} $(which python) -u ./conv2d_v2.py \
+            ${sruncmd} ${profilestring} $(which python) -u ./lstm1d_v2.py \
                 --dtype float${prec} \
                 --data_format ${data_format} \
                 --input_tensor_shape ${batch_size} ${input_tensor_shape} \
